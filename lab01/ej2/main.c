8ea4f9ddaa5a10711f818966aa8c800f08001e03
Miqueas <miqueasgigena18@gmail.com>
2025-04-03 15:17:44 -0300

    git feat: implementaciones en lab01 ejercicio 2, me tira error en archivo.o xq no tengo WSL

--- lab01/ej2/main.c
+++ lab01/ej2/main.c
@@ -66,5 +66,7 @@ int main(int argc, char *argv[]) {
 
     /* check if it is a permutation of original */
     assert(array_is_permutation_of(copy, array, length));
+
+    quick_sort(array,length);
     return EXIT_SUCCESS;
 }
