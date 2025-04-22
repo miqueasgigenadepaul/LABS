9984e533b3a94aa799e76d27b7b6b9b9c86fc066
miqueasgigenadepaul <miqueasgigena18@gmail.com>
2025-04-22 01:05:15 +0000

    git feat: agregamos tests de lab2 ej1 k_esimo

--- lab02/lab02/ej1/main.c
+++ lab02/lab02/ej1/main.c
@@ -3,9 +3,9 @@
 #include "k_esimo.h"
 
 int main(void) {
-    int a[] = {8, -2, 9, 0, 13};
+    int a[] = {-2, 8, 9, 0, 13};
     int length = 5;
-    int k = 1;
+    int k = 2;
     int result;
 
     result = k_esimo(a, length, k);
--- lab02/lab02/ej1/tests.c
+++ lab02/lab02/ej1/tests.c
@@ -3,7 +3,7 @@
 #include "k_esimo.h"
 
 #define MAX_LENGTH 5
-#define N_TESTCASES 1
+#define N_TESTCASES 10
 
 struct testcase {
     int a[MAX_LENGTH];
@@ -16,8 +16,16 @@ int main() {
     struct testcase tests[N_TESTCASES] = {
       // arreglo, largo, k, resultado esperado
       { {8}, 1, 0, 8 },
+      { {8,9}, 2, 0, 8},
+      { {8,9}, 2, 1, 9},
+      { {3,1}, 2, 0, 1},
+      { {3,1}, 2, 1, 3},
+      { {8, -2, 9, 0, 13}, 5, 0, -2},
+      { {8, -2, 9, 0, 13}, 5, 1, 0},
+      { {8, -2, 9, 0, 13}, 5, 2, 8},
+      { {8, -2, 9, 0, 13}, 5, 3, 9},
+      { {8, -2, 9, 0, 13}, 5, 4, 13},
 
-      // COMPLETAR!!
     };
     int result;
 
